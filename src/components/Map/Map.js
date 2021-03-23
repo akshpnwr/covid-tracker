633851bec235d83541dfdf1180e5d297f5e8678d
akshpnwr <akashpanwar9976@gmail.com>
2021-03-23 21:06:14 +0530

    getting current location and centering the map to it

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,43 @@
 import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
+import { Component } from 'react';
 
-const map = () => {
-  return (
-    <div id="map">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
-  );
-};
+class Map extends Component {
+  state = {
+    lat: null,
+    lon: null,
+  };
 
-export default map;
+  componentDidMount() {
+    console.log('mount');
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+      });
+    });
+  }
+
+  render() {
+    let map = <h1>Loading...</h1>;
+
+    if (this.state.lat && this.state.lon) {
+      map = (
+        <MapContainer
+          center={[this.state.lat, this.state.lon]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+      );
+    }
+    return <div id="map">{map}</div>;
+  }
+}
+
+export default Map;
