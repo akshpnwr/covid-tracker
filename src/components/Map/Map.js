1a9fd378fc0c534abb507f22a236f98bfd3434b5
akshpnwr <akashpanwar9976@gmail.com>
2021-03-25 20:34:05 +0530

    Adding a popup

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -2,41 +2,39 @@ import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 import { Component } from 'react';
+import icon from './Icon/Icon';
 
 class Map extends Component {
   state = {
-    lat: null,
-    lon: null,
+    pos: [],
   };
 
   componentDidMount() {
     console.log('mount');
     navigator.geolocation.getCurrentPosition((position) => {
       this.setState({
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
+        pos: [position.coords.latitude, position.coords.longitude],
       });
     });
   }
 
   render() {
-    let map = <h1>Loading...</h1>;
+    let map = <h1 style={{ color: 'black' }}>Loading...</h1>;
 
-    if (this.state.lat && this.state.lon) {
+    if (this.state.pos.length) {
       map = (
-        <MapContainer
-          center={[this.state.lat, this.state.lon]}
-          zoom={13}
-          scrollWheelZoom={false}
-        >
-          {/* <TileLayer
+        <MapContainer center={this.state.pos} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          /> */}
-          <TileLayer
+          />
+          {/* <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-          />
+          /> */}
+          <Marker icon={icon} position={this.state.pos}>
+            <Popup>A pretty CSS3 popup.</Popup>
+          </Marker>
         </MapContainer>
       );
     }
