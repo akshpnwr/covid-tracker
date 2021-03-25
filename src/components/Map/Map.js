b42bc35c161ab95e88f07cb92aae1bee3155c628
akshpnwr <akashpanwar9976@gmail.com>
2021-03-25 20:02:24 +0530

    Changing the map design

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -29,9 +29,13 @@ class Map extends Component {
           zoom={13}
           scrollWheelZoom={false}
         >
-          <TileLayer
+          {/* <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          /> */}
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           />
         </MapContainer>
       );
